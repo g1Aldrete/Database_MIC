119d6af83f1aff609096fe460dc5408c31997885
g1Aldrete <60672465+g1Aldrete@users.noreply.github.com>
2023-11-12 21:33:33 -0600

    Add files via upload

---
+++ DataBase CC/back_end/connection_code/credentials.js
@@ -0,0 +1,9 @@
+
+//My credentials to connect to the data base//
+//GABRIELA ALDRETE 
+const dbConfig = {
+    user: "gxa2206",
+    password: "Cse5333Cloud",
+    connectString: "acaddbprod.uta.edu:1523/pcse1p.data.uta.edu"
+}
+module.exports = dbConfig;
